d275c05304720cf2445e3247674c0b3a89b53145
Paul Schur <paul.schur@gmx.de>
2024-08-19 18:11:41 +0200

    main script

---
+++ js/app.js
@@ -0,0 +1,84 @@
+class App {
+    constructor(root_id, data_url) {
+        this.root_element = document.getElementById(root_id);
+        this.data_url = data_url;
+
+        window.addEventListener('hashchange', () => {
+            this.render();
+        });
+    }
+
+    data() {
+        return fetch(this.data_url).then(response => response.json());
+    }
+
+    url(url) {
+        return '#' + url;
+    }
+
+    build_element(data) {
+        let element = document.createElement(data.type);
+
+        if (data.id) {
+            element.id = data.id;
+        }
+
+        if (data.class) {
+            element.className = data.class;
+        }
+
+        if (data.text) {
+            element.innerHTML = data.text;
+        }
+
+        if (data.children) {
+            data.children.forEach(child => {
+                element.appendChild(this.build_element(child));
+            });
+        }
+
+        if (data.attributes) {
+            data.attributes.forEach(attribute => {
+                element.setAttribute(attribute.name, attribute.value);
+            });
+        }
+
+        return element
+    }
+
+    getHash() {
+        let hash = window.location.hash.slice(1);
+
+        if (hash === '') {
+            return '/';
+        }
+
+        return hash;
+    }
+
+    render() {
+        this.data().then(this.render_content.bind(this));
+
+        this.root_element.innerHTML = `
+            <h1>Hello World</h1>
+        `;
+    }
+
+    render_content(data) {
+        let content = data.pages[this.getHash()];
+
+        if (!content) {
+            content = data.pages['/404'];
+        }
+
+        this.render_page(content);
+    }
+
+    render_page(page) {
+        document.title = page.title;
+        let content = this.build_element(page);
+
+        this.root_element.innerHTML = '';
+        this.root_element.appendChild(content);
+    }
+}
